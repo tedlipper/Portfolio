837febd6c1b06549ceb1888fa74e1d383f23ea86
tedlipper <49607317+tedlipper@users.noreply.github.com>
2023-11-08 09:22:54 -0800

    Update ReferenceScriptCalculator.cs

--- Portfolio/C#/ReferenceScriptCalculator.cs
+++ Portfolio/C#/ReferenceScriptCalculator.cs
@@ -19,14 +19,14 @@ namespace BetterCalculator
                 //Converting the numbers inputed into a double also using variables
                 Console.Write("Enter a number: ");
                 double num1 = Convert.ToDouble(Console.ReadLine());
-
+                //Asking for an operator
                 Console.Write("Enter Operator (+, -, *, or /): ");
                 string op = Console.ReadLine();
-
+                //Asking for another number
                 Console.Write("Enter another number: ");
                 double num2 = Convert.ToDouble(Console.ReadLine());
 
-                //If else statements to match the printed outcome to the operator given
+                //If else statements to print the outcome to the operator and numbers given
                 if (op == "+")
                 {
                     Console.WriteLine("The answer is " + (num1 + num2));
